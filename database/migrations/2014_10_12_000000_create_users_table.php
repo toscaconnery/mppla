99ee6d7f487bdccd6d6c6354dceba49e4100d0e1
toscayconnery <tosca14@mhs.if.its.ac.id>
2017-05-10 22:32:49 +0700

    update user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,16 @@ class CreateUsersTable extends Migration
             $table->integer('id_lab')->nullable();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('no_hp');->nullable();
             $table->string('password');
             $table->integer('is_admin')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->string('tempat_lahir')->nullable();
+            $table->integer('is_surabaya')->nullable();
+            $table->string('alamat_asal')->nullable();
+            $table->string('alamat_surabaya')->nullable();
+            $table->string('agama')->nullable();
+            $table->string('motivasi')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
